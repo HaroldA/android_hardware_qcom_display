b1f1f0b48362ac5a26063ac63c9ac25a2c9630ee
HaroldA <harold9701@gmail.com>
2017-12-15 18:41:13 +0530

    Add msm8974

--- Android.mk
+++ Android.mk
@@ -6,6 +6,10 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     #This is for 8084 based platforms
     include $(call all-named-subdir-makefiles,msm8084)
   else
+    ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+      #This is for 8974 based (and B-family) platforms
+      include $(call all-named-subdir-makefiles,msm8974)
+    else
     ifneq ($(filter msm8226 msm8x26,$(TARGET_BOARD_PLATFORM)),)
       include $(call all-named-subdir-makefiles,msm8226)
     else
@@ -23,6 +27,7 @@ ifneq ($(TARGET_BOARD_AUTO),true)
             else
               ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
                 include $(call all-named-subdir-makefiles,msm8998)
+                endif
               endif
             endif
           endif
